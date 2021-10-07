fff16b3fc2defb5f4dded7e8070867a8fe34d317
saman <saman_waruka@cmu.ac.th>
2021-10-07 15:34:52 +0700

    feat: add consumer get message with spacific message offset

---
+++ src/consumer-with-spacific-message.ts
@@ -0,0 +1,34 @@
+// this example for get all messages from message offset number until last message
+
+import { Consumer, KafkaClient } from 'kafka-node';
+
+const client = new KafkaClient({ kafkaHost: 'localhost:9092' });
+const consumer = new Consumer(
+  client,
+  [{ topic: 'topic1', partition: 0, offset: 1 }],
+  // offset ไว้กำหนดว่าจะอ่านตั้งแต่ message offset 1
+  {
+    autoCommit: false, //  true คือ เมื่อได้รับ message จะ  set message เป็น commit message อัตโนมัติ
+    encoding: 'utf8',
+    fromOffset: true // true คือ ดึง message ทั้งหมดตั้งแต่เริ่ม มา   false คือ  ดึงแค่ message ที่ยังไม่ได้ commit
+  }
+);
+
+consumer.on('message', function (message) {
+  console.log('consumer1 ==> message', message);
+});
+
+consumer.on('error', function (err) {
+  console.log('error occur ', err);
+});
+
+consumer.on('offsetOutOfRange', function (err) {
+  console.log('offsetOutOfRange occur ', err);
+});
+
+// consumer.commit(function (err, data) {
+//   if (err) {
+//     console.log('commit error', err);
+//   }
+//   console.log('commit Data', data);
+// });
